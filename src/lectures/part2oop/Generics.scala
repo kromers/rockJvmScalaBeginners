3ab4c96902682a6100c3190207d9dd27e0aa0d42
kromers <javitrapero@gmail.com>
2019-08-29 19:15:15 +0200

    learning

--- src/lectures/part2oop/Generics.scala
+++ src/lectures/part2oop/Generics.scala
@@ -3,9 +3,14 @@ package lectures.part2oop
 
 object Generics extends App {
 
-  class MyList[A] {
+  class MyList[+A] {
     // use the type A
+    def add[B >: A](element: B): MyList[B] = ???
 
+    /*
+      A = Cat
+      B = Animal
+     */
   }
 
 
@@ -29,14 +34,23 @@ object Generics extends App {
   class CovariantList[+A]
   val animal: Animal = new Cat
   val animalList: CovariantList[Animal] = new CovariantList[Cat]
-  // animalList.add(new Dog) ???   HARD QUESTION
+  //animalList.add(new Dog) //???   HARD QUESTION  => We return a list of Animals
 
   // 2. NO = INVARIANCE
   class InvariantList[A]
   val invariantList: InvariantList[Animal] = new InvariantList[Animal]
 
   // 3. HELL, no CONTRAVARIANCE
-  class ContravariantList[-A]
-  val contravariantList: ContravariantList[Cat] = new ContravariantList[Animal]
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
 
+
+  //bounded types
+  class Cage[A <: Animal] (animal: A)
+  val cage = new Cage(new Dog)
+
+  class Car //extends Animal
+  //val newCage = new Cage(new Car) // no se queja el compilador, pero no compila porque Car no es una subclase de Animal
+
+  // expand Mylist to be a generic
 }
